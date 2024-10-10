9780bf87eb4bf333b8c6a81b2a04a9e6cd877b73
LautaroAraya <107148067+LautaroAraya@users.noreply.github.com>
2024-10-10 15:29:07 -0300

    Actualización de la entidad Hora y su vista
    - Eliminadas semillas de datos de la entidad `Hora` en `InstitutoContext.cs`.
    - Añadidos controles `DateTimePicker` y etiquetas en `AgregarEditarHoraView.Designer.cs` para seleccionar horas de inicio y fin.
    - Actualizados métodos en `AgregarEditarHoraView.cs` para manejar los nuevos controles de tiempo.
    - Modificada la clase `Hora` eliminando la propiedad `nombre` y la interfaz `IEntityIdNombre`. La propiedad `Nombre` ahora se calcula dinámicamente.
    - Realizados ajustes menores en `AgregarEditarHoraView.resx` relacionados con la serialización de objetos.

--- InstitutoServices/Models/Horarios/Hora.cs
+++ InstitutoServices/Models/Horarios/Hora.cs
@@ -6,7 +6,19 @@ namespace InstitutoServices.Models.Horarios
     public class Hora
     {
         public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
+        
+        [NotMapped]
+        public string Nombre
+        {
+            get { return EsRecreo?"Recreo":""+
+                    $"{Desde.Hour}:" +
+                    $"{Desde.Minute} - " +
+                    $"{Hasta.Hour}:" +
+                    $"{Hasta.Minute}"; }
+        }
+
+        public DateTime Desde { get; set; } = DateTime.MinValue;
+        public DateTime Hasta { get; set; } = DateTime.MinValue;
         public bool EsRecreo { get; set; } = false;
         public bool Eliminado { get; set; } = false;
 
