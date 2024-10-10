7f17c5e723fba276ebd2b2c2a5b6901585a11d36
LautaroAraya <107148067+LautaroAraya@users.noreply.github.com>
2024-10-10 14:52:46 -0300

    Mejoras en autenticación y limpieza de código
    - Se ha mejorado la clase `Hora` para que la propiedad `Nombre` devuelva un formato de hora específico o "Recreo" si corresponde.
    - Se ha limpiado el archivo del proyecto `InstitutoWeb.csproj` eliminando una carpeta innecesaria.
    - Se ha mejorado la página de inicio de sesión `Login.razor` añadiendo nuevas dependencias y botones para registrarse y autenticarse con Google.
    - Se ha añadido una opción para recordar la contraseña en el formulario de inicio de sesión.
    - Se ha añadido lógica para verificar la confirmación del correo electrónico del usuario antes de permitir el inicio de sesión.
    - Se ha añadido la funcionalidad para iniciar sesión con Google y registrarse desde la página de inicio de sesión.
    - Se ha actualizado el servicio `FirebaseAuthService` para manejar el inicio de sesión con Google y recordar la contraseña utilizando `localStorage` o `sessionStorage`.
    - Se ha actualizado la configuración de Firebase en `index.html` con nuevas credenciales.
    - Se ha añadido la funcionalidad para iniciar sesión con Google en `index.html`.
    - Se ha añadido un script para manejar `sessionStorage` en `index.html`.
    - Se ha añadido una nueva clase `FirebaseUser` en el archivo `FirebaseUser.cs` dentro del espacio de nombres `InstitutoServices.Models.Login`.
    - Se ha creado una nueva página Razor llamada `Registrarse.razor` con un formulario de registro.
    - Se ha añadido un nuevo archivo `sessionStorage.js` que define objetos para manejar `localStorage` y `sessionStorage`.

--- InstitutoServices/Models/Horarios/Hora.cs
+++ InstitutoServices/Models/Horarios/Hora.cs
@@ -6,19 +6,8 @@ namespace InstitutoServices.Models.Horarios
     public class Hora
     {
         public int Id { get; set; }
-
-        [NotMapped]
-        public string Nombre
-        {
-            get
-            {
-                // formateo la hora y minutos para que se muestren con 2 digitos si son cero
-                return $"{Desde.Hour:D2}:{Desde.Minute:D2} - {Hasta.Hour:D2}:{Hasta.Minute:D2}";
-            }
-        }
-
-        public DateTime Desde { get; set; } = DateTime.MinValue;
-        public DateTime Hasta { get; set; } = DateTime.MinValue;
+        public string Nombre { get; set; } = string.Empty;
+        public bool EsRecreo { get; set; } = false;
         public bool Eliminado { get; set; } = false;
 
 
